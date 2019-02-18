435ed845e8f12dfd0815786653c3aa4e458326fd
khirulbasher <khirulbasher@gmail.com>
2019-02-18 16:46:57 +0600

    Version Updated

--- src/main/java/com/lemon/idea/plugin/text/impl/ToLowerFlatSnakeCase.java
+++ src/main/java/com/lemon/idea/plugin/text/impl/ToLowerFlatSnakeCase.java
@@ -1,10 +1,10 @@
 package com.lemon.idea.plugin.text.impl;
 
-import com.lemon.framework.textprocessing.text.impl.ToLowerSnakeCaseProcessor;
+import com.lemon.framework.textprocessing.text.impl.ToLowerFlatSnakeCaseProcessor;
 import com.lemon.idea.plugin.text.TextAction;
 
 public class ToLowerFlatSnakeCase extends TextAction {
     public ToLowerFlatSnakeCase() {
-        super(new ToLowerSnakeCaseProcessor());
+        super(new ToLowerFlatSnakeCaseProcessor());
     }
 }
